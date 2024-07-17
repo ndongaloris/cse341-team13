712550a3c67dff98b71201ca1cd808e7c8c2bbb6
Koigor97 <turaysamuel101@yahoo.com>
2024-07-17 14:31:25 +0200

    refactor the degree schema

--- models/degree.js
+++ models/degree.js
@@ -1,51 +1,48 @@
 module.exports = (mongoose) => {
-  const Degree = mongoose.model(
-    "Degree", // Model name should be singular and capitalized
-    mongoose.Schema({
-      name: {
-        type: String,
-        required: [true, "Degree name is required"],
-        minlength: [3, "Degree name must be at least 3 characters long"],
-      },
-      institution: {
-        type: String,
-        /**
-         * ? This field is should be a reference to the institution model
-         * ? In real-world application, degrees and institutions have a natural
-         * ? relationship.
-         */
-      },
-      type: {
-        type: String,
-        required: [true, "Degree type is required"],
-        enum: ["Associate", "Bachelor"],
-      },
-      description: {
-        type: String,
-      },
-      potentialEmployment: {
-        type: [String], //* this indicates a list of potential employments
-      },
-      duration: {
-        type: String,
-        required: [true, "Degree duration is required"],
-        match: [
-          /^\d+ (years?|months?)$/,
-          "Duration should be in the format '10 years' or '3 months'",
-        ],
-      },
-      creditsRequired: {
-        type: Number,
-        required: [true, "Credits required is mandatory"],
-        min: [1, "Minimum credits required is 1"],
-      },
-      level: {
-        type: String,
-        required: [true, "Degree level is required"],
-        enum: ["Undergraduate", "Graduate"],
-      },
-    })
-  );
+  const DegreeSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "Degree name is required"],
+      minlength: [3, "Degree name must be at least 3 characters long"],
+    },
+    institution: {
+      type: String,
+      /**
+       * ? This field is should be a reference to the institution model
+       * ? In real-world application, degrees and institutions have a natural
+       * ? relationship.
+       */
+    },
+    type: {
+      type: String,
+      required: [true, "Degree type is required"],
+      enum: ["Associate", "Bachelor"],
+    },
+    description: {
+      type: String,
+    },
+    potentialEmployment: {
+      type: [String], //* this indicates a list of potential employments
+    },
+    duration: {
+      type: String,
+      required: [true, "Degree duration is required"],
+      match: [
+        /^\d+ (years?|months?)$/,
+        "Duration should be in the format '10 years' or '3 months'",
+      ],
+    },
+    creditsRequired: {
+      type: Number,
+      required: [true, "Credits required is mandatory"],
+      min: [1, "Minimum credits required is 1"],
+    },
+    level: {
+      type: String,
+      required: [true, "Degree level is required"],
+      enum: ["Undergraduate", "Graduate"],
+    },
+  });
 
   /**
    * * To prevent duplicate degrees with the same name and type
@@ -55,7 +52,12 @@ module.exports = (mongoose) => {
    * * without conflicts.
    *
    */
-  Degree.index({ institution: 1, name: 1, type: 1 }, { unique: true });
+  DegreeSchema.index({ institution: 1, name: 1, type: 1 }, { unique: true });
+
+  /**
+   * Defines the Degree model.
+   */
+  const Degree = mongoose.model("Degree", DegreeSchema);
 
   return Degree;
 };

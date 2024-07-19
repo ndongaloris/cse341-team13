dfc114980d98d28bcb20b7a870d4b4c656645647
Koigor97 <turaysamuel101@yahoo.com>
2024-07-19 15:41:48 +0200

    add * to make comment highlight

--- routers/institution.js
+++ routers/institution.js
@@ -2,42 +2,42 @@ const router = require("express").Router();
 const institutionController = require("../controllers/institutions");
 const { isAuthenticated } = require("../middleware/authenticate");
 /**
- * Route to get all institutions.
- * GET /
- * Calls institutionController.getAll function.
+ * * Route to get all institutions.
+ * * GET /
+ * * Calls institutionController.getAll function.
  */
 router.get("/", institutionController.getAll);
 
 /**
- * Route to get a single institution by ID.
- * GET /:id
- * Calls institutionController.getSingle function.
- * @param {string} id - The ID of the institution to retrieve.
+ * * Route to get a single institution by ID.
+ * * GET /:id
+ * * Calls institutionController.getSingle function.
+ * * @param {string} id - The ID of the institution to retrieve.
  */
 router.get("/:id", institutionController.getSingle);
 
 /**
- * Route to create a new institution.
- * POST /create
- * Calls institutionController.createInstitution function.
- * Expects JSON body with institution details.
+ * * Route to create a new institution.
+ * * POST /create
+ * * Calls institutionController.createInstitution function.
+ * * Expects JSON body with institution details.
  */
 router.post("/create", isAuthenticated, institutionController.createInstitution);
 
 /**
- * Route to update an institution by ID.
- * PUT /update/:id
- * Calls institutionController.updateInstitution function.
- * @param {string} id - The ID of the institution to update.
- * Expects JSON body with updated institution details.
+ * * Route to update an institution by ID.
+ * * PUT /update/:id
+ * * Calls institutionController.updateInstitution function.
+ * * @param {string} id - The ID of the institution to update.
+ * * Expects JSON body with updated institution details.
  */
 router.put("/update/:id", isAuthenticated, institutionController.updateInstitution);
 
 /**
- * Route to delete an institution by ID.
- * DELETE /delete/:id
- * Calls institutionController.deleteInstitution function.
- * @param {string} id - The ID of the institution to delete.
+ * * Route to delete an institution by ID.
+ * * DELETE /delete/:id
+ * * Calls institutionController.deleteInstitution function.
+ * * @param {string} id - The ID of the institution to delete.
  */
 router.delete("/delete/:id", isAuthenticated, institutionController.deleteInstitution);
 
